361742b634a3a77ec0b620f753f701bbec89123f
abrarasif11 <faasif929@gmail.com>
2025-08-26 22:30:31 +0600

    social-login

--- src/Component/SocialLogin/SocialLogin.jsx
+++ src/Component/SocialLogin/SocialLogin.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
 
 const SocialLogin = () => {
   return (
     <div>
-      <h2 className='text-2xl font-semibold'>Login With</h2>
-      <button className='btn'>Login With Google</button>
-      <button className='btn'>Login With Github</button>
+      <h2 className="text-2xl font-semibold mb-3">Login With</h2>
+      <div className="*:w-full space-y-2 ">
+        <button className="btn">
+          <FaGoogle />
+          Login With Google
+        </button>
+        <button className="btn">
+          <FaGithub />
+          Login With Github
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialLogin
+export default SocialLogin;
